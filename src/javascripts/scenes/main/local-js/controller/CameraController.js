bd58cbde85d94e5bdeb9900c396aa9d388382a6e
Jamaal Sawyer-Dymski <jamaal.sawyer-dymski@thegameagency.com>
2020-02-01 23:21:12 -0500

    fix-frameskip

--- src/javascripts/scenes/main/local-js/controller/CameraController.js
+++ src/javascripts/scenes/main/local-js/controller/CameraController.js
@@ -1,40 +1,39 @@
 class CameraController {
-    constructor(scene) {
-        this.scene = scene;
-        //set to transparent after debug
-        this._center = scene.add.rectangle(0,0,5, 5, 0x000000, 1);
-        scene.cameras.main.startFollow(this._center);
-        //TODO reset center to 2000;?
-        scene.cameras.main.setBounds(-500, 0, 1975, 500)
-        //scene.cameras.main.bounds = null;
+  constructor(scene) {
+    this.scene = scene;
+    //set to transparent after debug
+    this._center = scene.add.rectangle(0, 0, 5, 5, 0x000000, 1);
+    scene.cameras.main.startFollow(this._center);
+    //TODO reset center to 2000;?
+    scene.cameras.main.setBounds(-500, 0, 1975, 500);
+    //scene.cameras.main.bounds = null;
 
+  }
+  UpdatePosition(fighters, scene) {
+    let x = 0;
+    let y = 0;
+    for(let i = 0; i < fighters.length; ++i) {
+      x += fighters[i].x;
+      y += fighters[i].y;
     }
-    UpdatePosition(fighters, scene){
-        
-        x = 0;
-        y = 0;
-        for (i = 0; i < fighters.length; ++i){
-            x += fighters[i].x;
-            y += fighters[i].y;
-        }
-        x /= fighters.length
-        y /= fighters.length
+    x /= fighters.length;
+    y /= fighters.length;
 
-        newX = this.lerp(this._center.x, x, 0.5);
-        newY = this.lerp(this._center.y, y, 0.5);
-        this._center.setPosition(newX, newY);
+    const newX = this.lerp(this._center.x, x, 0.5);
+    const newY = this.lerp(this._center.y, y, 0.5);
+    this._center.setPosition(newX, newY);
 
 
-    }
-    UpdateWorldBounds(scene){
-        const { centerX, centerY, width, height } = scene.cameras.main;
-        x = scene.cameras.main.scrollX + centerX
-        y = scene.cameras.main.scrollY + centerY
-        scene.physics.world.setBounds(x - (width / 2), y - (height / 2), width, height);
-    }
+  }
+  UpdateWorldBounds(scene) {
+    const { centerX, centerY, width, height } = scene.cameras.main;
+    const x = scene.cameras.main.scrollX + centerX;
+    const y = scene.cameras.main.scrollY + centerY;
+    scene.physics.world.setBounds(x - (width / 2), y - (height / 2), width, height);
+  }
 
-    lerp(a, b, t){
-        return (a + t*(b - a));
-    }
+  lerp(a, b, t) {
+    return (a + t * (b - a));
+  }
 }
 module.exports = CameraController;
--- src/javascripts/scenes/main/local-js/controller/MainSceneController.js
+++ src/javascripts/scenes/main/local-js/controller/MainSceneController.js
@@ -10,8 +10,9 @@ class MainSceneController {
     this.ui = this.layers.ui;
     this.cameraController = scene._gameVars.cameraController;
     this.cameraController.UpdatePosition(this.fighters, this.scene);
-    this.cameraController.UpdateWorldBounds(this.scene);
+    // this.cameraController.UpdateWorldBounds(this.scene);
     this.ui.onUpdate(this.scene);
+    this.skipFrames = 60;
   }
 
   onUpdate(time, delta) {
@@ -22,15 +23,20 @@ class MainSceneController {
     this.scene.physics.world.collide(this.fighters, this.floor);
     this.scene.physics.world.collide(this.fighters[0], this.fighters[1]);
     this.stateMachines.forEach(sm => sm.step());
-    this.cameraController.UpdatePosition(this.fighters, this.scene)
-    this.cameraController.UpdateWorldBounds(this.scene);
+    this.cameraController.UpdatePosition(this.fighters, this.scene);
+    if(this.skipFrames > 0) {
+      this.skipFrames--;
+    } else {
+      this.cameraController.UpdateWorldBounds(this.scene);
+    }
+
     //this.ui.onUpdate(this.scene);
 
-    if (Phaser.Input.Keyboard.JustDown(spacebar)) {
+    if(Phaser.Input.Keyboard.JustDown(spacebar)) {
       //this.takeDamage("fighter1", this.fighters[0], this.fighters[0].damage);
-      this.ui.Shake()
+      this.ui.Shake();
     }
-    if (Phaser.Input.Keyboard.JustDown(tab)){
+    if(Phaser.Input.Keyboard.JustDown(tab)) {
       //this.takeDamage("fighter2", this.fighters[1], this.fighters[1].damage);
       this.ui.Unshake();
     }
@@ -50,7 +56,7 @@ class MainSceneController {
     if(facing !== side) {
       this.fighters[0].facing = this.fighters[1].facing;
       this.fighters[1].facing = facing;
-      this.fighters[0]._classVars.sprite.scaleX *= -1; 
+      this.fighters[0]._classVars.sprite.scaleX *= -1;
       this.fighters[1]._classVars.sprite.scaleX *= -1;
       this.fighters[0]._classVars.attackOneHitbox.setOrigin(this.fighters[0]._classVars.attackOneHitbox.originX === 0 ? 1 : 0, 0.5);
       this.fighters[1]._classVars.attackOneHitbox.setOrigin(this.fighters[1]._classVars.attackOneHitbox.originX === 0 ? 1 : 0, 0.5);
@@ -68,19 +74,19 @@ class MainSceneController {
       this.reportHit(this.stateMachines[other].key);
       const fighter = this.fighters[other];
       fighter.health += this.fighters[index].damage;
-      if (fighter.health >= fighter.config.health){
+      if(fighter.health >= fighter.config.health) {
         this.endGame();
       }
       console.log(fighter.health);
       console.log(fighter.config.health);
-      this.ui.UpdateBar(this.stateMachines[other].key, fighter.health/ fighter.config.health);
+      this.ui.UpdateBar(this.stateMachines[other].key, fighter.health / fighter.config.health);
     }
   }
   reportHit(key) {
     console.log(key);
   }
-  endGame(){
-    alert("Game Over");
+  endGame() {
+    alert('Game Over');
   }
 }
 
