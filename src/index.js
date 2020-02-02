13fedbc5abb8a718d0e4fc948bd6865f65ae31fa
Jamaal Sawyer-Dymski <jamaal.sawyer-dymski@thegameagency.com>
2020-02-02 11:27:35 -0500

    add game-end scene

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const defaultConfig = require('./javascripts/config/game');
 const MainScene = require('./javascripts/scenes/main');
+const GameEndScene = require('./javascripts/scenes/game-end');
 
 const config = {
   ...defaultConfig,
   scene: [
     MainScene,
+    GameEndScene,
   ]
 };
 
