f9a68ed0cbfb2b86dfa89a7dbf907888700243c2
Jamaal Sawyer-Dymski <jamaal.sawyer-dymski@thegameagency.com>
2020-02-02 11:22:10 -0500

    update preload

--- src/javascripts/scenes/intro/preload.js
+++ src/javascripts/scenes/intro/preload.js
@@ -1,11 +1,5 @@
 const preload = function() {
   console.log('preload');
-  this.load.glsl('starfields', 'shaders/starfields.glsl.js');
-  this.load.image('logo', 'images/logo.png');
-  this.load.image('mainBackground', 'images/main_back.png');
-  this.load.atlas('doctors', 'images/doctors.png', 'images/doctors.json');
-  this.load.image('ui_back', 'images/topui.png');
-  this.load.image('bar', 'images/fillbar.png');
 };
 
 module.exports = preload;
