b43c248b45f584de310f93f65ba2afa833066cc9
Jamaal Sawyer-Dymski <jamaal.sawyer-dymski@thegameagency.com>
2020-02-01 22:21:05 -0500

    more fixes

--- src/javascripts/scenes/main/local-js/fighter/states/AttackOneState.js
+++ src/javascripts/scenes/main/local-js/fighter/states/AttackOneState.js
@@ -2,7 +2,7 @@ const State = require('../../../../../shared/state-machine/State');
 class AttackOneState extends State {
   enter(scene, stateParams, enterParams) {
     const { fighter } = stateParams;
-    fighter.body.setVelocity(0, 0);
+    
     fighter.body.setAcceleration(0, 0);
     fighter.playAnim('attack1');
     const { attackOneHitbox} = fighter._classVars;
--- src/javascripts/scenes/main/local-js/fighter/states/HitState.js
+++ src/javascripts/scenes/main/local-js/fighter/states/HitState.js
@@ -6,16 +6,20 @@ class HitState extends State {
     const facingLeft = fighter.facing === 'left';
     const { jumpVel, jumpMaxVel } = fighter.config;
     fighter.body.setAcceleration(0, 0);
-    // fighter.body.setVelocityY(-jumpVel.y);
+    fighter.body.setVelocityY(-jumpVel.y);
+
     const velX = facingLeft ? jumpVel.x : -jumpVel.x;
+    
     fighter.body.setVelocityX(velX);
+    console.log(jumpVel);
+    fighter.body.setVelocityY(-jumpVel.y);
     fighter.body.setMaxVelocity(jumpMaxVel.x, jumpMaxVel.y);
+ 
     fighter.playAnim('hit');
     const { attackOneHitbox } = fighter._classVars;
     attackOneHitbox.setVisible(false);
     fighter.setDepth(5);
     scene.time.delayedCall(500, () => {
-      fighter.setInvincible();
       this.stateMachine.transition('idle');
       fighter.setDepth(1);
     });
